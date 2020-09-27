80137b5a220116d4d97e3643a06ead304d0efcd0
ryansamuelhust <69859659+ryansamuelhust@users.noreply.github.com>
2020-09-27 13:44:23 -0700

    Created pokemonList variable
    Added three objects to the pokemonList array. Each object has three shared keys with values sourced from pokemon.com

--- scripts.js
+++ scripts.js
@@ -1,3 +1,8 @@
-alert('Hello World');
-let favoriteFood = 'Soup' ;
-document.write (favoriteFood) ;
+let pokemonList = [
+  { Name: 'Bulbasaur', height: 2, types: ['grass', 'poison']
+  },
+  { Name: 'Charmander', height: 2, types: ['fire']
+  },
+  { Name: 'Squirtle', height: 1.5, types: ['water']
+  },
+];
